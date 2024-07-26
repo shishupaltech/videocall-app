2b257c77cd5a196c9bce8e0bca0fa79228b94ba2
shishupaltech <veerutech75@gmail.com>
2024-07-26 14:07:41 +0530

    changes done

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, {
-    cors: {
-        origin: "*",
-    },
-});
+const io = socketIo(server);
+
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 io.on('connection', (socket) => {
-    console.log('New user connected:', socket.id);
+    console.log('New client connected');
 
     socket.on('callUser', ({ userToCall, signalData, from, name, id }) => {
-        io.to(userToCall).emit('callUser', { from, name, signal: signalData, id });
+        io.to(userToCall).emit('callUser', { signal: signalData, from, name, id });
     });
 
-    socket.on('answerCall', ({ signal, to, id }) => {
-        io.to(to).emit('callAccepted', signal);
+    socket.on('answerCall', (data) => {
+        io.to(data.to).emit('callAccepted', data.signal);
     });
 
     socket.on('endCall', ({ id }) => {
-        io.emit('callEnded', { id });
+        io.to(id).emit('callEnded');
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
+        console.log('Client disconnected');
     });
 });
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Handles any requests that don't match the above routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
