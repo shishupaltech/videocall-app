51daab781c6fdaff1c8613b4dbed6c75f7cb171a
shishupaltech <veerutech75@gmail.com>
2024-07-26 18:35:57 +0530

    close

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on ports ${PORT}`));
